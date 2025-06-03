8569d1d410e4294b5a20938fb819cdd5ed6e0977
2imPusc <khaphuc24@gmail.com>
2025-06-04 00:00:49 +0700

    FE: Start build

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css';
+
+function App() {
+  return (
+    <>
+    <div className="bg-blue-500 text-white p-4">
+      Kiểm tra Tailwind CSS
+    </div>
+    <div className="container mx-auto p-4"></div>
+    <h1 className="text-3xl font-bold mb-4">Hello, World!</h1>
+    </>
+  );
+}
+export default App;
